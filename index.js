cc5682e05459cb26e4956cb51c25063fc24e8f9e
Thierry Nowak <thinow@gmail.com>
2017-04-22 18:35:53 +0200

    Allow every origins

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/status', function(request, response) {
   response.send({ status: 'OK' });
 });
