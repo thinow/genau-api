8a3c17e67b6447b38cd1953464ed78c09cb721e3
Thierry Nowak <thinow@gmail.com>
2017-04-26 23:29:53 +0200

    randomized hard-coded values

--- index.js
+++ index.js
@@ -13,6 +13,29 @@ app.get('/api/status', function(request, response) {
   response.send({ status: 'OK' });
 });
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min +1)) + min;
+}
+
+function pickFrom(array) {
+  const index = getRandomIntInclusive(0, array.length);
+  return array[index];
+}
+
+app.get('/api/question/article', function(request, response) {
+  const data = [
+    { category: 'article', label: 'Tier', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+    { category: 'article', label: 'Tisch', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+    { category: 'article', label: 'Stuhl', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+    { category: 'article', label: 'Fenster', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+    { category: 'article', label: 'Stimmung', options: [{ value: 'der' }, { value: 'die', correct: true }, { value: 'das' }] }
+  ];
+
+  response.send(pickFrom(data));
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
