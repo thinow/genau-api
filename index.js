8e7a88e0dcbd5d46717d0fad5b2bb56129cd1edb
Thierry Nowak <thinow@gmail.com>
2017-04-29 23:49:20 +0200

    plural, preterit and translations

--- index.js
+++ index.js
@@ -26,19 +26,37 @@ function pickFrom(array) {
 
 app.get('/api/question/article', function(request, response) {
   response.send(pickFrom([
-    { category: 'article', label: 'Tier', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
-    { category: 'article', label: 'Tisch', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
-    { category: 'article', label: 'Stuhl', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
-    { category: 'article', label: 'Fenster', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
-    { category: 'article', label: 'Stimmung', options: [{ value: 'der' }, { value: 'die', correct: true }, { value: 'das' }] }
+    { category: 'article', label: 'Tier', translation: 'animal', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+    { category: 'article', label: 'Tisch', translation: 'table', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+    { category: 'article', label: 'Stuhl', translation: 'chair', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+    { category: 'article', label: 'Fenster', translation: 'window', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+    { category: 'article', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'der' }, { value: 'die', correct: true }, { value: 'das' }] }
+  ]));
+});
+
+app.get('/api/question/plural', function(request, response) {
+  response.send(pickFrom([
+    { category: 'plural', label: 'Tier', translation: 'animal', options: [{ value: 'die Tiere', correct: true }, { value: 'die Tier' }, { value: 'die Tieren' }, { value: 'die Tiern' }] },
+    { category: 'plural', label: 'Tisch', translation: 'table', options: [{ value: 'die Tische', correct: true }, { value: 'die Tisch' }, { value: 'die Tischen' }, { value: 'die Tischn' }] },
+    { category: 'plural', label: 'Stuhl', translation: 'chair', options: [{ value: 'die Stühle', correct: true }, { value: 'die Stühlen' }, { value: 'die Stuhle' }, { value: 'die Stuhlen' }] },
+    { category: 'plural', label: 'Fenster', translation: 'window', options: [{ value: 'die Fenster', correct: true }, { value: 'die Fenstern' }, { value: 'die Fensteren' }, { value: 'die Fenstere' }] },
+    { category: 'plural', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'die Stimmungen', correct: true }, { value: 'die Stimmung' }, { value: 'die Stimümnge' }, { value: 'die Stimümngen' }] }
   ]));
 });
 
 app.get('/api/question/perfect', function(request, response) {
   response.send(pickFrom([
-    { category: 'perfect', label: 'machen', options: [{ value: 'er hat gemacht', correct: true }, { value: 'er ist gemacht' }, { value: 'er hat gemachen' }, { value: 'er ist gemachen' }] },
-    { category: 'perfect', label: 'tun', options: [{ value: 'er hat getan', correct: true }, { value: 'er hat getun' }, { value: 'er hat tan' }, { value: 'er hat tun' }] },
-    { category: 'perfect', label: 'vergessen', options: [{ value: 'er hat vergessen', correct: true }, { value: 'er hat vergesst' }, { value: 'er hat vergegessen' }, { value: 'er hat vergegesst' }] }
+    { category: 'perfect', label: 'machen', translation: 'to do, to make', options: [{ value: 'er hat gemacht', correct: true }, { value: 'er ist gemacht' }, { value: 'er hat gemachen' }, { value: 'er ist gemachen' }] },
+    { category: 'perfect', label: 'tun', translation: 'to do', options: [{ value: 'er hat getan', correct: true }, { value: 'er hat getun' }, { value: 'er hat tan' }, { value: 'er hat tun' }] },
+    { category: 'perfect', label: 'vergessen', translation: 'to forget', options: [{ value: 'er hat vergessen', correct: true }, { value: 'er hat vergesst' }, { value: 'er hat vergegessen' }, { value: 'er hat vergegesst' }] }
+  ]));
+});
+
+app.get('/api/question/preterit', function(request, response) {
+  response.send(pickFrom([
+    { category: 'preterit', label: 'machen', translation: 'to do, to make', options: [{ value: 'er machte', correct: true }, { value: 'er macht' }, { value: 'er mochte' }, { value: 'er möchte' }] },
+    { category: 'preterit', label: 'tun', translation: 'to do', options: [{ value: 'er tut', correct: true }, { value: 'er tan' }, { value: 'er tute' }, { value: 'er tante' }] },
+    { category: 'preterit', label: 'vergessen', translation: 'to forget', options: [{ value: 'er vergaß', correct: true }, { value: 'er vergiß' }, { value: 'er vergesst' }, { value: 'er vergesset' }] }
   ]));
 });
 
