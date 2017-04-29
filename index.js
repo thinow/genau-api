1b1687c52fec294e2b11a3e9bee193f3204de8a8
Thierry Nowak <thinow@gmail.com>
2017-04-29 23:58:40 +0200

    provide all endpoint

--- index.js
+++ index.js
@@ -13,6 +13,36 @@ app.get('/api/status', function(request, response) {
   response.send({ status: 'OK' });
 });
 
+const ARTICLE = [
+  { category: 'article', label: 'Tier', translation: 'animal', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+  { category: 'article', label: 'Tisch', translation: 'table', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+  { category: 'article', label: 'Stuhl', translation: 'chair', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
+  { category: 'article', label: 'Fenster', translation: 'window', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
+  { category: 'article', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'der' }, { value: 'die', correct: true }, { value: 'das' }] }
+];
+
+const PLURAL = [
+  { category: 'plural', label: 'Tier', translation: 'animal', options: [{ value: 'die Tiere', correct: true }, { value: 'die Tier' }, { value: 'die Tieren' }, { value: 'die Tiern' }] },
+  { category: 'plural', label: 'Tisch', translation: 'table', options: [{ value: 'die Tische', correct: true }, { value: 'die Tisch' }, { value: 'die Tischen' }, { value: 'die Tischn' }] },
+  { category: 'plural', label: 'Stuhl', translation: 'chair', options: [{ value: 'die Stühle', correct: true }, { value: 'die Stühlen' }, { value: 'die Stuhle' }, { value: 'die Stuhlen' }] },
+  { category: 'plural', label: 'Fenster', translation: 'window', options: [{ value: 'die Fenster', correct: true }, { value: 'die Fenstern' }, { value: 'die Fensteren' }, { value: 'die Fenstere' }] },
+  { category: 'plural', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'die Stimmungen', correct: true }, { value: 'die Stimmung' }, { value: 'die Stimümnge' }, { value: 'die Stimümngen' }] }
+];
+
+const PERFECT = [
+  { category: 'perfect', label: 'machen', translation: 'to do, to make', options: [{ value: 'er hat gemacht', correct: true }, { value: 'er ist gemacht' }, { value: 'er hat gemachen' }, { value: 'er ist gemachen' }] },
+  { category: 'perfect', label: 'tun', translation: 'to do', options: [{ value: 'er hat getan', correct: true }, { value: 'er hat getun' }, { value: 'er hat tan' }, { value: 'er hat tun' }] },
+  { category: 'perfect', label: 'vergessen', translation: 'to forget', options: [{ value: 'er hat vergessen', correct: true }, { value: 'er hat vergesst' }, { value: 'er hat vergegessen' }, { value: 'er hat vergegesst' }] }
+];
+
+const PRETERIT = [
+  { category: 'preterit', label: 'machen', translation: 'to do, to make', options: [{ value: 'er machte', correct: true }, { value: 'er macht' }, { value: 'er mochte' }, { value: 'er möchte' }] },
+  { category: 'preterit', label: 'tun', translation: 'to do', options: [{ value: 'er tut', correct: true }, { value: 'er tan' }, { value: 'er tute' }, { value: 'er tante' }] },
+  { category: 'preterit', label: 'vergessen', translation: 'to forget', options: [{ value: 'er vergaß', correct: true }, { value: 'er vergiß' }, { value: 'er vergesst' }, { value: 'er vergesset' }] }
+];
+
+const ALL = Array.concat(ARTICLE, PLURAL, PERFECT, PRETERIT);
+
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -25,39 +55,23 @@ function pickFrom(array) {
 }
 
 app.get('/api/question/article', function(request, response) {
-  response.send(pickFrom([
-    { category: 'article', label: 'Tier', translation: 'animal', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
-    { category: 'article', label: 'Tisch', translation: 'table', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
-    { category: 'article', label: 'Stuhl', translation: 'chair', options: [{ value: 'der', correct: true }, { value: 'die' }, { value: 'das' }] },
-    { category: 'article', label: 'Fenster', translation: 'window', options: [{ value: 'der' }, { value: 'die' }, { value: 'das', correct: true }] },
-    { category: 'article', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'der' }, { value: 'die', correct: true }, { value: 'das' }] }
-  ]));
+  response.send(pickFrom(ARTICLE));
 });
 
 app.get('/api/question/plural', function(request, response) {
-  response.send(pickFrom([
-    { category: 'plural', label: 'Tier', translation: 'animal', options: [{ value: 'die Tiere', correct: true }, { value: 'die Tier' }, { value: 'die Tieren' }, { value: 'die Tiern' }] },
-    { category: 'plural', label: 'Tisch', translation: 'table', options: [{ value: 'die Tische', correct: true }, { value: 'die Tisch' }, { value: 'die Tischen' }, { value: 'die Tischn' }] },
-    { category: 'plural', label: 'Stuhl', translation: 'chair', options: [{ value: 'die Stühle', correct: true }, { value: 'die Stühlen' }, { value: 'die Stuhle' }, { value: 'die Stuhlen' }] },
-    { category: 'plural', label: 'Fenster', translation: 'window', options: [{ value: 'die Fenster', correct: true }, { value: 'die Fenstern' }, { value: 'die Fensteren' }, { value: 'die Fenstere' }] },
-    { category: 'plural', label: 'Stimmung', translation: 'atmosphere', options: [{ value: 'die Stimmungen', correct: true }, { value: 'die Stimmung' }, { value: 'die Stimümnge' }, { value: 'die Stimümngen' }] }
-  ]));
+  response.send(pickFrom(PLURAL));
 });
 
 app.get('/api/question/perfect', function(request, response) {
-  response.send(pickFrom([
-    { category: 'perfect', label: 'machen', translation: 'to do, to make', options: [{ value: 'er hat gemacht', correct: true }, { value: 'er ist gemacht' }, { value: 'er hat gemachen' }, { value: 'er ist gemachen' }] },
-    { category: 'perfect', label: 'tun', translation: 'to do', options: [{ value: 'er hat getan', correct: true }, { value: 'er hat getun' }, { value: 'er hat tan' }, { value: 'er hat tun' }] },
-    { category: 'perfect', label: 'vergessen', translation: 'to forget', options: [{ value: 'er hat vergessen', correct: true }, { value: 'er hat vergesst' }, { value: 'er hat vergegessen' }, { value: 'er hat vergegesst' }] }
-  ]));
+  response.send(pickFrom(PERFECT));
 });
 
 app.get('/api/question/preterit', function(request, response) {
-  response.send(pickFrom([
-    { category: 'preterit', label: 'machen', translation: 'to do, to make', options: [{ value: 'er machte', correct: true }, { value: 'er macht' }, { value: 'er mochte' }, { value: 'er möchte' }] },
-    { category: 'preterit', label: 'tun', translation: 'to do', options: [{ value: 'er tut', correct: true }, { value: 'er tan' }, { value: 'er tute' }, { value: 'er tante' }] },
-    { category: 'preterit', label: 'vergessen', translation: 'to forget', options: [{ value: 'er vergaß', correct: true }, { value: 'er vergiß' }, { value: 'er vergesst' }, { value: 'er vergesset' }] }
-  ]));
+  response.send(pickFrom(PRETERIT));
+});
+
+app.get('/api/question/all', function(request, response) {
+  response.send(pickFrom(ALL));
 });
 
 app.listen(app.get('port'), function() {
