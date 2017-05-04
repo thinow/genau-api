89ebbb800963ca1963575100eda4d649d364398f
Thierry Nowak <thinow@gmail.com>
2017-05-04 20:28:12 +0200

    es6: use nodemon

--- index.es6.js
+++ index.es6.js
@@ -2,7 +2,7 @@ import http from 'http';
 
 http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello {{YOUR_NAME_HERE}}\n');
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
